3240442952925bb3d97637d325c9f02c06cd076f
lucx <chaoxin.lu@pistonint.com>
2024-03-30 22:08:11 +0800

    修改构建践镜像目标内核架构

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
-prefix=gzv-reg.xyz.com/library/auth
-version="v1.2.0"
+prefix=gzv-reg.piston.ink/library/auth
+version="v1.1.0"
 
-docker buildx build --platform linux/amd64 -f Dockerfile-Build -t "${prefix}"/oauth2-proxy:"${version}" . --push &&
-docker buildx build --platform linux/amd64 -f Dockerfile-Build -t "${prefix}"/oauth2-proxy:latest . --push
+docker buildx build --platform linux/arm64 -f Dockerfile-Build -t "${prefix}"/oauth2-proxy:"${version}" . --push &&
+docker buildx build --platform linux/arm64 -f Dockerfile-Build -t "${prefix}"/oauth2-proxy:latest . --push
